c1db32011c370729d7d2e8012a592ba1a128e54e
Inno stacks <innostatcks@gmail.com>
2024-07-15 23:57:07 +0530

    minor changes

--- App.tsx
+++ App.tsx
@@ -103,7 +103,7 @@ function App(): React.JSX.Element {
 
       <MyComponentWithLogger message={usrPref.getHelloworld()} />
       <Mycomp2 fullName={usrPref.getusrName()} />
-      <Text>Hello new world testing</Text>
+      <Text>Hello new world testing 123</Text>
     </View>
   );
 }
