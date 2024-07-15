efb3df5fe96ad8109bbe256d25d0f4f531ac0771
Inno stacks <innostatcks@gmail.com>
2024-07-15 23:06:37 +0530

    yml file changes

--- App.tsx
+++ App.tsx
@@ -103,7 +103,7 @@ function App(): React.JSX.Element {
 
       <MyComponentWithLogger message={usrPref.getHelloworld()} />
       <Mycomp2 fullName={usrPref.getusrName()} />
-      <Text>Hello new world</Text>
+      <Text>Hello new world 123</Text>
     </View>
   );
 }
