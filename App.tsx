41fbda79fb97a60f36cb0852390c0fd461ab2d11
Inno stacks <innostatcks@gmail.com>
2024-07-18 18:44:37 +0530

    added app distribution

--- App.tsx
+++ App.tsx
@@ -103,7 +103,7 @@ function App(): React.JSX.Element {
 
       <MyComponentWithLogger message={usrPref.getHelloworld()} />
       <Mycomp2 fullName={usrPref.getusrName()} />
-      <Text>Hello new world testing</Text>
+      <Text>Welcome to hello wrold</Text>
     </View>
   );
 }
